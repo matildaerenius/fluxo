7bf8d1f38a5aab54848ab5cb7426c599179b0947
Matilda Erenius <matildaerenius@hotmail.com>
2025-10-10 15:05:09 +0200

    feat(backend): init Spring Boot

---
+++ backend/src/test/java/se/matildaerenius/fluxo/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.matildaerenius.fluxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
