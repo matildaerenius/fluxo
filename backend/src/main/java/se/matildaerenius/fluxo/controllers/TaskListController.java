f431defd2963ab694df0d1d58fe417498731c5aa
Matilda Erenius <matildaerenius@hotmail.com>
2025-10-11 20:41:46 +0200

    feat(controller): add TaskListController with GET /task-lists endpoint

---
+++ backend/src/main/java/se/matildaerenius/fluxo/controllers/TaskListController.java
@@ -0,0 +1,31 @@
+package se.matildaerenius.fluxo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.matildaerenius.fluxo.domain.dto.TaskListDto;
+import se.matildaerenius.fluxo.mappers.TaskListMapper;
+import se.matildaerenius.fluxo.services.TaskListService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task-lists")
+public class TaskListController {
+
+    private final TaskListService taskListService;
+    private final TaskListMapper taskListMapper;
+
+    public TaskListController(TaskListService taskListService, TaskListMapper taskListMapper) {
+        this.taskListService = taskListService;
+        this.taskListMapper = taskListMapper;
+    }
+
+    @GetMapping
+    public List<TaskListDto> listTaskLists() {
+        return taskListService.listTasksLists()
+                .stream()
+                .map(taskListMapper::toDto)
+                .toList();
+    }
+}
