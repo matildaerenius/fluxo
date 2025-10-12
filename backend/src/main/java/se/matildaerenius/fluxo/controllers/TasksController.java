c6931bb9ddfb95cc0066aabdcd2ed1d1d8c33607
Matilda Erenius <matildaerenius@hotmail.com>
2025-10-12 18:58:34 +0200

    feat(tasks): add POST /task-lists/{id}/tasks and service create with validation

--- backend/src/main/java/se/matildaerenius/fluxo/controllers/TasksController.java
+++ backend/src/main/java/se/matildaerenius/fluxo/controllers/TasksController.java
@@ -1,10 +1,8 @@
 package se.matildaerenius.fluxo.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import se.matildaerenius.fluxo.domain.dto.TaskDto;
+import se.matildaerenius.fluxo.domain.entities.Task;
 import se.matildaerenius.fluxo.mappers.TaskMapper;
 import se.matildaerenius.fluxo.services.TaskService;
 
@@ -12,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(path= "/task-lists/{task_list_id}/tasks")
+@RequestMapping(path = "/task-lists/{task_list_id}/tasks")
 public class TasksController {
 
     private final TaskService taskService;
@@ -29,5 +27,16 @@ public class TasksController {
                 .stream()
                 .map(taskMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public TaskDto createTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @RequestBody TaskDto taskDto) {
+        Task createdTask = taskService.createTask(
+                taskListId,
+                taskMapper.fromDto(taskDto)
+        );
+        return taskMapper.toDto(createdTask);
     }
 }
--- backend/src/main/java/se/matildaerenius/fluxo/services/impl/TaskServiceImpl.java
+++ backend/src/main/java/se/matildaerenius/fluxo/services/impl/TaskServiceImpl.java
@@ -2,23 +2,62 @@ package se.matildaerenius.fluxo.services.impl;
 
 import org.springframework.stereotype.Service;
 import se.matildaerenius.fluxo.domain.entities.Task;
+import se.matildaerenius.fluxo.domain.entities.TaskList;
+import se.matildaerenius.fluxo.domain.entities.TaskPriority;
+import se.matildaerenius.fluxo.domain.entities.TaskStatus;
+import se.matildaerenius.fluxo.repositories.TaskListRepository;
 import se.matildaerenius.fluxo.repositories.TaskRepository;
 import se.matildaerenius.fluxo.services.TaskService;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskListRepository taskListRepository;
 
-    public TaskServiceImpl(TaskRepository taskRepository) {
+    public TaskServiceImpl(TaskRepository taskRepository, TaskRepository taskPriorityRepository, TaskListRepository taskListRepository) {
         this.taskRepository = taskRepository;
+        this.taskListRepository = taskListRepository;
     }
 
     @Override
     public List<Task> listTasks(UUID taskListId) {
         return taskRepository.findByTaskListId(taskListId);
+    }
+
+    @Override
+    public Task createTask(UUID taskListId, Task task) {
+        if (null != task.getId()) {
+            throw new IllegalArgumentException("Task already has an ID!");
+        }
+        if (null == task.getTitle() || task.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Task must have a title!");
+        }
+        TaskPriority taskPriority = Optional.ofNullable(task.getPriority())
+                .orElse(TaskPriority.MEDIUM);
+
+        TaskStatus taskStatus = TaskStatus.OPEN;
+
+        TaskList taskList = taskListRepository.findById(taskListId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Task List ID provided!"));
+
+        LocalDateTime now = LocalDateTime.now();
+        Task taskToSave = new Task(
+                null,
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                taskStatus,
+                taskPriority,
+                taskList,
+                now,
+                now
+        );
+        return taskRepository.save(taskToSave);
     }
 }
