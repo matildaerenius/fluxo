9295c7f59d3922ee999b99fa554f2c3b41be0d06
Matilda Erenius <matildaerenius@hotmail.com>
2026-03-04 13:06:49 +0100

    feat: switch Retrofit converter from Gson to Kotlinx serialization

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.retrofit)
-    implementation(libs.retrofit.converter.gson)
+    implementation(libs.retrofit.converter.kotlinx)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.navigation.compose.v297)
     implementation(libs.kotlinx.serialization.json)
